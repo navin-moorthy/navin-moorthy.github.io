20d9ebabb885175e8855822ab0ec693369effaa4
NavinNavi19 <navin007.a@gmail.com>
2019-06-15 10:25:13 +0000

    Linting Errors Fixed

--- src/styles/animations.jsx
+++ src/styles/animations.jsx
@@ -1,5 +1,5 @@
-import tw from 'tailwind.macro'
-import styled, { keyframes, css } from 'styled-components'
+import tw from "tailwind.macro";
+import styled, { keyframes, css } from "styled-components";
 
 const rotate = keyframes`
   from {
@@ -8,7 +8,7 @@ const rotate = keyframes`
   to {
     transform: rotate(360deg);
   }
-`
+`;
 
 const wave = keyframes`
   0% {
@@ -20,7 +20,7 @@ const wave = keyframes`
   100% {
     d: path("M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z");
   }
-`
+`;
 
 const upDown = keyframes`
   from {
@@ -29,7 +29,7 @@ const upDown = keyframes`
   to {
     transform: translateY(30px);
   }
-`
+`;
 
 const upDownWide = keyframes`
   from {
@@ -38,30 +38,30 @@ const upDownWide = keyframes`
   to {
     transform: translateY(200px);
   }
-`
+`;
 
 const upDownAnimation = css`
   ${upDown} 4s ease-in-out infinite alternate;
-`
+`;
 
 const upDownWideAnimation = css`
   ${upDownWide} 18s ease-in-out infinite alternate;
-`
+`;
 
 export const UpDown = styled.div`
   animation: ${upDownAnimation};
   ${tw`pin absolute`};
-`
+`;
 
 export const UpDownWide = styled.div`
   animation: ${upDownWideAnimation};
   ${tw`pin absolute`};
-`
+`;
 
 export const waveAnimation = length => css`
   animation: ${wave} ${length} linear infinite alternate;
-`
+`;
 
 export const rotateAnimation = length => css`
   animation: ${rotate} ${length} linear infinite;
-`
+`;
