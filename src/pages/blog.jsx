feca8e9c072f6d8b2c74870dd3e7daa37cdacd38
NavinNavi19 <navin007.a@gmail.com>
2019-06-16 20:18:57 +0000

    Markdown Home Blog Ready Basic

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,16 +1,85 @@
 import React from "react";
-import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
+import tw from "tailwind.macro";
 
-const Blog = () => {
+import Layout from "../components/Layout";
+
+const BlogStyle = tw.div`font-sans ml-auto text-base md:text-xl mr-auto max-w-4xl px-5 py-10 `;
+
+const BlogHeader = tw.h1`font-serif text-5xl text-center tracking-wider leading-normal mb-10 mt-0`;
+
+const HeaderLink = styled(props => <Link {...props} />)`
+  ${tw`no-underline shadow-none text-white`}
+  text-shadow: 3px 4px black;
+`;
+
+const PostsHeader = tw.h3`mb-0 mt-6 text-3xl `;
+
+const Small = tw.small`text-sm`;
+
+const PostPara = tw.p`p-0 mx-0 mt-2 mb-8`;
+
+const PostsLink = styled(props => <Link {...props} />)`
+  ${tw`no-underline shadow-none`}
+`;
+
+const Blog = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges;
+
   return (
     <>
-      <div style={{ color: `teal` }}>
-        <h1>About Gatsby</h1>
-        <p>Such wow. Very React.</p>
-      </div>
-      <Link to="/">Home</Link>
+      <Layout siteTitle="Blog" />
+      <BlogStyle>
+        <BlogHeader>
+          <HeaderLink to="/">Navin's technical blog</HeaderLink>
+        </BlogHeader>
+        {posts.map(({ node }) => {
+          const { id, excerpt, fields, frontmatter } = node;
+          const { title, date, description } = frontmatter;
+          return (
+            <div key={id}>
+              <PostsHeader>
+                <PostsLink to={`/blog${fields.slug}`}>{title}</PostsLink>
+              </PostsHeader>
+              <Small>{date}</Small>
+              <PostPara
+                dangerouslySetInnerHTML={{
+                  __html: description || excerpt
+                }}
+              />
+            </div>
+          );
+        })}
+      </BlogStyle>
     </>
   );
 };
 
 export default Blog;
+
+Blog.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
