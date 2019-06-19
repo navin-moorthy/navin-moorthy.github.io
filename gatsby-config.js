15b53271cb1d8a6c7aa0f649d797599baca9eb32
NavinNavi19 <navin007.a@gmail.com>
2019-06-19 06:41:23 +0000

    Sitemap URL Fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,11 +98,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
-        output: "/some-other-sitemap.xml",
-        // Exclude specific pages or groups of pages using glob parameters
-        // See: https://github.com/isaacs/minimatch
-        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-        exclude: [],
+        output: "/sitemap.xml",
         query: `
           {
             site {
